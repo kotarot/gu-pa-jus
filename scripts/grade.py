846a000a89129bef2d2116a1d626e4c0b8d62143
Kotaro Terada <kotarot@apache.org>
2020-07-25 20:06:37 +0900

    Cope with multiple external files

--- scripts/grade.py
+++ scripts/grade.py
@@ -142,18 +142,18 @@ def grade_source_code(filename, problem, grade_config):
         logging.info('    Could not compile the source code. --> score = {}'.format(score))
         return score
 
-    # 外部ファイルが指定されていればコピーする
-    if 'external_file' in problem_config:
-        student_dir = os.path.dirname(filename)
-        external_filename = '{}/../{}'.format(student_dir, problem_config['external_file'])
-        basename = os.path.basename(external_filename)
-        proc = subprocess.run('docker cp {} my-gu-pa-jus:/root/{}'.format(external_filename, basename).split(' '))
-
     # テストケースで実行する
     # 失敗するごとに5点から1点ずつ減らしていく (ただし設定ファイルに得点が指定されていればその点を引く)
     passed, failed, penalty = 0, 0, 0
     for i, test_case in enumerate(problem_config['test_cases']):
-        logging.info('    Trying test case {} ... '.format(i + 1))
+        logging.info('    Trying test case {} / {} ... '.format(i + 1, len(problem_config['test_cases'])))
+
+        # 外部ファイルが指定されていればコピーする
+        if 'external_file' in test_case:
+            student_dir = os.path.dirname(filename)
+            external_filename = '{}/../{}'.format(student_dir, test_case['external_file']['source'])
+            proc = subprocess.run('docker cp {} my-gu-pa-jus:/root/{}'.format(external_filename, test_case['external_file']['destination']).split(' '))
+
         try:
             proc = subprocess.run('docker exec -i my-gu-pa-jus /root/a.out'.split(' '),
                     input=test_case['input'], encoding='UTF-8',
